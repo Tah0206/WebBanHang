ab2309faf55b7f001ec92946fc9aa03017d85aad
Nguyễn Anh Đức <23DH110809@st.huflit.edu.vn>
2024-11-19 18:04:23 +0700

    GIOHANG

---
+++ WebBanHang/Models/ViewModel/CartService.cs
@@ -0,0 +1,33 @@
+﻿using WebBanHang.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebBanHang.Models.ViewModel;
+
+namespace WebBanHang.Models.ViewModel
+{
+    public class CartService
+    {
+
+        private readonly HttpSessionStateBase session;
+        public CartService(HttpSessionStateBase session)
+        {
+            this.session = session;
+        }
+        public Cart GetCart()
+        {
+            var cart = (Cart)session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public void ClearCart()
+        {
+            session["Cart"] = null;
+        }
+    }
+}
